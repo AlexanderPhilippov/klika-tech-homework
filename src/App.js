bbd4ce81ae0f4e837e514f3b0be62d824020e6ae
Unknown <alexphil@gmail.com>
2017-04-15 21:45:41 +0600

    add redux, filters, semantic, data from json object

--- src/App.js
+++ src/App.js
@@ -1,80 +1,107 @@
-import React, {Component} from 'react';
-import {Container, Header, Table, Icon, Grid, Dropdown, Label} from 'semantic-ui-react'
+import React from 'react';
+import {
+  Container,
+  Header,
+  Table,
+  Icon,
+  Grid,
+  Dropdown,
+  Label
+} from 'semantic-ui-react'
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    authorFilter: "all",
+    genreFilter: "all",
+    yearFilter: "all"
+  }
+  changeYearFilter = (e, {name, value}) => this.setState({yearFilter: value})
+  changeGenreFilter = (e, {name, value}) => this.setState({genreFilter: value})
+  changeAuthorFilter = (e, {name, value}) => this.setState({authorFilter: value})
+
   render() {
+    const {playList, filterData} = this.props
     return (
       <Container>
-          <Header as='h2' icon textAlign='center'>
-            <Icon name='music' circular/>
-            <Header.Content>
-              Музыка на любой вкус!
-            </Header.Content>
-          </Header>
-          <Grid divided="vertically">
+        <Header as='h2' icon textAlign='center'>
+          <Icon name='music' circular/>
+          <Header.Content>
+            Музыка на любой вкус!
+          </Header.Content>
+        </Header>
+        <Grid divided="vertically">
           <Grid.Row columns="2">
-          <Grid.Column width="13">
-          <Table sortable striped>
-            <Table.Header>
-              <Table.HeaderCell><Icon name="user"/>Исполнитель</Table.HeaderCell>
-              <Table.HeaderCell><Icon name="sound"/>Песня</Table.HeaderCell>
-              <Table.HeaderCell><Icon name="tag"/>Жанр</Table.HeaderCell>
-              <Table.HeaderCell><Icon name="calendar"/>Год</Table.HeaderCell>
-            </Table.Header>
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell>
-                  1234</Table.Cell>
-                <Table.Cell>
-                  <Icon name="trash"/>
-                  4234342</Table.Cell>
-                <Table.Cell>
-                  234234</Table.Cell>
-                <Table.Cell>
-                  234234234</Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>
-                  1234</Table.Cell>
-                <Table.Cell>
-                  <Icon name="trash"/>
-                  4234342</Table.Cell>
-                <Table.Cell>
-                  234234</Table.Cell>
-                <Table.Cell>
-                  234234234</Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>
-                  1234</Table.Cell>
-                <Table.Cell>
-                  <Icon name="trash"/>
-                  4234342</Table.Cell>
-                <Table.Cell>
-                  234234</Table.Cell>
-                <Table.Cell>
-                  234234234</Table.Cell>
-              </Table.Row>
-            </Table.Body>
-            <Table.Footer>
-              <Table.HeaderCell colSpan="4">Всего:</Table.HeaderCell>
-            </Table.Footer>
-          </Table>
-          </Grid.Column>
-          <Grid.Column width="3">
-          <Table>
-          <Table.Header><Table.HeaderCell><Icon name="filter"/>Фильтр</Table.HeaderCell></Table.Header>
-          <Table.Body>
-          <Table.Row><Table.Cell><Label ribbon color="pink">Исполнитель</Label><Dropdown fluid/></Table.Cell></Table.Row>
-          <Table.Row><Table.Cell><Label ribbon color="orange">Жанр</Label><Dropdown fluid/></Table.Cell></Table.Row>
-          <Table.Row><Table.Cell><Label ribbon color="blue">Год</Label><Dropdown fluid/></Table.Cell></Table.Row>
-          </Table.Body>
-          </Table>
-          </Grid.Column>
+            <Grid.Column width="13">
+              <Table sortable striped compact>
+                <Table.Header>
+                  <Table.HeaderCell><Icon name="user"/>Исполнитель</Table.HeaderCell>
+                  <Table.HeaderCell><Icon name="sound"/>Песня</Table.HeaderCell>
+                  <Table.HeaderCell><Icon name="tag"/>Жанр</Table.HeaderCell>
+                  <Table.HeaderCell><Icon name="calendar"/>Год</Table.HeaderCell>
+                </Table.Header>
+                <Table.Body>
+                  {playList !== undefined && playList.map(x => 
+                    ((this.state.yearFilter === 'all' || this.state.yearFilter === x.year) &&
+                      (this.state.genreFilter === 'all' || this.state.genreFilter === x.genre) &&
+                      (this.state.authorFilter === 'all' || this.state.authorFilter === x.author)) &&
+                    <Table.Row key={`${x.author}_${x.song}`}>
+                    <Table.Cell>{x.author}</Table.Cell>
+                    <Table.Cell>{x.song}</Table.Cell>
+                    <Table.Cell>{x.genre}</Table.Cell>
+                    <Table.Cell>{x.year}</Table.Cell>
+                  </Table.Row>)}
+                </Table.Body>
+                <Table.Footer>
+                  <Table.HeaderCell colSpan="4">Всего: {playList !== undefined && playList.length}</Table.HeaderCell>
+                </Table.Footer>
+              </Table>
+            </Grid.Column>
+            <Grid.Column width="3">
+              <Table>
+                <Table.Header>
+                  <Table.HeaderCell><Icon name="filter"/>Фильтр</Table.HeaderCell>
+                </Table.Header>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>
+                      <Label ribbon size="mini" color="violet">Исполнитель</Label><Dropdown
+                        fluid
+                        search
+                        selection
+                        closeOnChange
+                        options={filterData.authors}
+                        onChange={this.changeAuthorFilter}
+                        value={this.state.authorFilter}/></Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>
+                      <Label ribbon size="mini" color="teal">Жанр</Label><Dropdown
+                        fluid
+                        search
+                        selection
+                        closeOnChange
+                        options={filterData.genres}
+                        onChange={this.changeGenreFilter}
+                        value={this.state.genreFilter}/></Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>
+                      <Label ribbon size="mini" color="blue">Год</Label><Dropdown
+                        fluid
+                        search
+                        selection
+                        closeOnChange
+                        options={filterData.years}
+                        onChange={this.changeYearFilter}
+                        value={this.state.yearFilter}/></Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Grid.Column>
           </Grid.Row>
         </Grid>
       </Container>
-    );
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+
 import App from './App';
 import '../semantic/dist/semantic.min.css';
+import data from './data'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function storeManager(state = [], action) {
+  if (action.type === 'ADD_TO_PLAYLIST') {
+    return [
+      ...state,
+      action.track
+    ]
+  }
+  return (state)
+}
+const store = createStore(storeManager)
+  const initialFilter = {
+      text: "Все",
+      value: "all"
+    }
+    const years = [initialFilter],
+      genres = [initialFilter],
+      authors = [initialFilter]
+
+    // store.subscribe( () => console.log('Subscribe', store.getState()) )
+
+    data
+      .playList
+      .forEach(x => {
+        store.dispatch({type: 'ADD_TO_PLAYLIST', track: x})
+        years.push({text: x.year, value: x.year})
+        genres.push({text: x.genre, value: x.genre})
+        authors.push({text: x.author, value: x.author})
+      })
+
+    ReactDOM.render(
+      <App
+      playList={store.getState()}
+      filterData={{
+      years,
+      genres,
+      authors
+    }}/>, document.getElementById('root'));
